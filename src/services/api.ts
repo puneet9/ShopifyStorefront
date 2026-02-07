62eeec490f0b0bf7f34459851ddf56d50b07eec4
P9 <pnine@MacBook-Pro.local>
2026-02-07 15:12:17 -0500

    feat: update API URL to gist endpoint
    - Changed from cdn.shopify.com (404) to gist.githubusercontent.com
    - URL: https://gist.githubusercontent.com/agorovyi/40dcd166a38b4d1e9156ad66c87111b7/raw/36f1c815dd83ed8189e55e6e6619b5d7c7c4e7d6/testProducts.json
    - App now fetches real product data from the provided endpoint
    - Mock data retained as fallback

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { ShopifyProduct, Product, ProductVariant } from '../types';
 
 const API_URL =
-  'https://cdn.shopify.com/s/files/1/0533/2089/files/products.json?v=1613490589';
+  'https://gist.githubusercontent.com/agorovyi/40dcd166a38b4d1e9156ad66c87111b7/raw/36f1c815dd83ed8189e55e6e6619b5d7c7c4e7d6/testProducts.json';
 
 interface ProductsResponse {
   products: ShopifyProduct[];
